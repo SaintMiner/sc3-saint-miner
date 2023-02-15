ec8a7e75515063d4b847bbe329ec731ddd6d2ccc
SaintMiner <dinden290@gmail.com>
2023-02-15 20:59:09 +0200

    first commit

---
+++ miner.lua
@@ -0,0 +1,69 @@
+local export = {}
+
+local currentLevelStep = 1;
+local totalSteps = 1;
+local isEven = false;
+local level = 1;
+
+function digBlocks()
+    turtle.digUp();
+    while turtle.detect() do
+        turtle.dig();
+    end
+    turtle.digDown();
+end
+
+function doStep()
+    digBlocks();
+    turtle.forward();
+    totalSteps = totalSteps + 1;
+    currentLevelStep = currentLevelStep + 1;
+end
+
+function nextLevel()
+    for i = 1, 3 do
+        digBlocks();
+        turtle.down();
+    end
+    level = level + 1;
+end
+
+function getRotation(x, y)
+    local isBackloop = level % 2 == 0;
+    print(isBackloop);
+    if (isEven and isBackloop) then
+        return currentLevelStep % (x * 2) == 0 and 'turnLeft' or 'turnRight';
+    end
+    return currentLevelStep % (x * 2) == 0 and 'turnRight' or 'turnLeft';
+end
+
+function export.mine(x, y)
+    turtle.refuel();
+    if (y % 2 == 0) then
+        isEven = true;
+    end
+    print(x, y);
+    print('Even:', isEven)
+    while turtle.getFuelLevel() > 0 do
+        currentLevelStep = 1;
+        for i = 1, y do
+            for j = 1, (x - 1) do
+                doStep();
+            end
+            -- local rotating = currentLevelStep % (x * 2) == 0 and 'turnRight' or 'turnLeft';
+            local rotation = getRotation(x, y);
+
+            if currentLevelStep == x * y then
+                turtle[rotation]();
+                turtle[rotation]();
+                nextLevel();
+            else
+                turtle[rotation]();
+                doStep();
+                turtle[rotation]();
+            end
+        end
+    end
+end
+
+return export;
---
+++ work.lua
@@ -0,0 +1,33 @@
+local miner = require('miner');
+local dry = require('dry');
+
+local modem = peripheral.find('modem');
+modem.open(888);
+
+function worker()
+    while true do
+        local event = {os.pullEvent()};
+
+        if event[1] == "modem_message" then
+            local command = event[5];
+            local action = command[1];            
+
+            if action == "miner" then
+              local x = command[2];
+              local y = command[3];
+              miner.mine(x, y);
+            end
+
+            if action == "dry" then
+                local x = command[2];
+                local y = command[3];
+                dry.dry(x, y);
+            end
+        end
+    end
+end
+
+parallel.waitForAny(worker);
+-- pastebin get GeyZeZqr work.lua
+-- pastebin get x8p1FhQU miner.lua
+-- pastebin get XyeUnPJK startup.lua
